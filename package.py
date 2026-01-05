b61a361faa3e9a976e14842f10f6461b1598204c
OfficePigeon <ava@wich.fun>
2026-01-04 21:19:46 -0500

    Split into versions and fixed recipes

---
+++ package.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+
+for archive in os.listdir('builds'):
+	if archive.endswith('.zip'):
+		os.remove('builds/' + str(archive))
+for version in os.listdir('versions'):
+	shutil.make_archive('builds/BoneBlockShortcut' + str(version), 'zip', 'versions/' + str(version))
